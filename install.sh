15f47b7c0280f8712e8139d2a7d455ab81d2cb6a
Michael "Gilli" Gilliland <michael.gilliland@arcadia.io>
2019-06-27 08:25:21 -0400

    Add pathogen installation instruction.

--- install.sh
+++ install.sh
@@ -13,4 +13,6 @@ if [ "$dotfile" == "vimrc" ]; then
 	echo "Making ~/.vim/backup and ~/.vim/tmp directories"
 	mkdir -p ~/.vim/backup
 	mkdir -p ~/.vim/tmp
+	echo "Installing pathogen"
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
